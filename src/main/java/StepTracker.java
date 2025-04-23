62046d9baaa3def7ce8233d711932d1107ec6e18
Emily Pan <pancakerula@gmail.com>
2025-04-22 18:42:20 -0700

    it may have been the double type mismatch that backstabbed me, if so I apologize to the resilient ArrayList

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,45 +1,45 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- // private ArrayList <Integer> steps; //idk why this is causing errors
+ private ArrayList <Integer> steps; //idk why this is causing errors
  private int minSteps;
- private int steps;
- private int activeDays;
- private int days;
+ // private int steps;
+ // private int activeDays;
+ // private int days;
  
  
  public StepTracker(int a){
-  // steps = new ArrayList <Integer>();
+  steps = new ArrayList <Integer>();
   minSteps = a;
-  steps = 0;
-  activeDays = 0;
-  days = 0;
+  // steps = 0;
+  // activeDays = 0;
+  // days = 0;
  }
  public void addDailySteps(int step){
-  // steps.add(step);
-  steps += step;
-  days++;
-  if(step >= minSteps){
-   activeDays++;
-  }
+  steps.add(step);
+  // steps += step;
+  // days++;
+  // if(step >= minSteps){
+  //  activeDays++;
+  // }
   
  }
  public double averageSteps(){
-  // int b = 0;
-  // for(int i : steps){
-  //  b += i;
-  // }
-  // return (double)b/steps.size();
-  return (double)steps/days;
+  double b = 0;
+  for(int i : steps){
+   b += i;
+  }
+  return (double)b/steps.size();
+  // return (double)steps/days;
  }
  public int activeDays(){
-  // int c = 0;
-  // for(int i : steps){
-  //  if(i >= minSteps){
-  //   c++;
-  //  }
-  // }
-  // return c;
-  return activeDays;
+  int c = 0;
+  for(int i : steps){
+   if(i >= minSteps){
+    c++;
+   }
+  }
+  return c;
+  // return activeDays;
  }
 } 
